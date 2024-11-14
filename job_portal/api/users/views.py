53c646ada2c86204265386289e4cebf5a884573c
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-14 13:15:08 +0530

    create a view for user registration

--- job_portal/api/users/views.py
+++ job_portal/api/users/views.py
@@ -1,10 +1,45 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserRegisterSerializer
 
 
 # Create your views here.
-class UserAPIView(APIView):
+class RegisterView(APIView):
     """User Operations"""
 
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(
+                {"message": "User registered successfully."},
+                status=status.HTTP_201_CREATED,
+            )
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    """user logout functionality"""
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return JsonResponse(
+                {"message": "Logout Successfully"}, status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProtectedView(APIView):
+    authentication_classes = [JWTAuthentication]  # Use JWT authentication
+    permission_classes = [permissions.IsAuthenticated]  # Allow only authenticated users
+
     def get(self, request):
-        return JsonResponse({"data": "working"})
+        content = {"message": "Successful!"}
+        return JsonResponse(content)
