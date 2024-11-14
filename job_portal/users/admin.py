c03164974d3803d4ec417c98268fc7e687618984
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-14 17:28:47 +0530

    added fields

--- job_portal/users/admin.py
+++ job_portal/users/admin.py
@@ -6,14 +6,28 @@ from .models import User, ApplicantProfile, Skill
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["id", "first_name", "last_name", "username", "email"]
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "is_applicant",
+        "is_employer",
+    ]
     search_fields = ["username", "email", "id"]
     list_filter = ["last_login", "date_joined"]
 
 
 @admin.register(ApplicantProfile)
 class ApplicantProfileAdmin(admin.ModelAdmin):
-    list_display = ["id", "full_name"]
+    list_display = [
+        "user",
+        "phone_number",
+        "address",
+        "resume_file",
+        "profile_complete",
+    ]
 
 
 @admin.register(Skill)
