b38943adb993413155ec1b2028a68b6702f1617d
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-14 13:15:26 +0530

    added urls for jwt tokens

--- job_portal/api/urls.py
+++ job_portal/api/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
-    path("users/", include("api.users.urls")),
+    path("user/", include("api.users.urls")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
