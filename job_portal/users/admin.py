5388fc6ff6c846f1d8aa6b8843e5dbba62315e43
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-14 13:16:42 +0530

    register User, ApplicantProfile, Skill

--- job_portal/users/admin.py
+++ job_portal/users/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from .models import User
+from .models import User, ApplicantProfile, Skill
 
 # Register your models here.
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["id", "username", "email"]
+    list_display = ["id", "first_name", "last_name", "username", "email"]
+    search_fields = ["username", "email", "id"]
+    list_filter = ["last_login", "date_joined"]
+
+
+@admin.register(ApplicantProfile)
+class ApplicantProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "full_name"]
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+    ]
