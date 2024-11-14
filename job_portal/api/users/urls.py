4440050375bbb095a76944f49b55589a20dda8dd
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-14 13:14:44 +0530

    added urls for register, login, logout

--- job_portal/api/users/urls.py
+++ job_portal/api/users/urls.py
@@ -1,7 +1,11 @@
 from . import views
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path("", views.UserAPIView.as_view(), name="all_users"),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("protected-view/", views.ProtectedView.as_view(), name="protected-view"),
 ]
