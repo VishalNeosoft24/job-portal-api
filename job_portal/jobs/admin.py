d9c6b9c1df11346d0e3820d125ea1647ba344efd
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-15 14:49:53 +0530

    register models

--- job_portal/jobs/admin.py
+++ job_portal/jobs/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Jobs, JobApplication
 
 # Register your models here.
+
+
+@admin.register(Jobs)
+class JobsAdmin(admin.ModelAdmin):
+    """Jobs Admin"""
+
+    list_display = [
+        "id",
+        "employer",
+        "job_title",
+        "description",
+        "location",
+        "salary_min",
+        "salary_max",
+        "job_type",
+        "experience_level",
+        "posted_date",
+        "is_active",
+    ]
+    list_filter = [
+        "job_title",
+        "location",
+        "salary_min",
+        "salary_max",
+        "experience_level",
+        "posted_date",
+        "is_active",
+    ]
+
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    """JobApplicationAdmin"""
+
+    list_display = [
+        "id",
+        "applicant",
+        "job_listing",
+        "status",
+        "applied_date",
+    ]
+    list_filter = ["status"]
