905d05efd7dfcf9079f9231fd5597ccf4ae15256
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-15 15:33:34 +0530

    create JobApplication urls

--- job_portal/api/jobs/urls.py
+++ job_portal/api/jobs/urls.py
@@ -3,7 +3,21 @@ from . import views
 
 
 urlpatterns = [
+    # Jobs
     path("job/", views.JobCreateRetrieveView.as_view(), name="job"),
-    path("employer/job/", views.RetrieveEmployerJob.as_view(), name="job"),
-    path("job/<int:job_id>/", views.JobRetrieveUpdateDeleteView.as_view(), name="job"),
+    path("employer/job/", views.RetrieveEmployerJob.as_view(), name="employer_job"),
+    path(
+        "job/<int:job_id>/",
+        views.JobRetrieveUpdateDeleteView.as_view(),
+        name="job_details",
+    ),
+    # JobApplication
+    path(
+        "job-application/", views.JobApplicationView.as_view(), name="job_application"
+    ),
+    path(
+        "job-application/<int:job_application_id>/",
+        views.JobApplicationView.as_view(),
+        name="job_application_",
+    ),
 ]
