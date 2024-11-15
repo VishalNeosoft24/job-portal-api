f8ef6ad0827d0e18d43f15938e4cdb46a12a0ce1
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2024-11-15 14:51:44 +0530

    added job urls

--- job_portal/api/urls.py
+++ job_portal/api/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("user/", include("api.users.urls")),
+    path("jobs/", include("api.jobs.urls")),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
